eb4b5488c68843ffcde4b0dc3b58c2925e4302f4
tornike2002 <toko.toka2000@gmail.com>
2025-06-03 23:06:19 +0400

    add cors origin

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,10 @@ import commentRoutes from './routes/comment'
 import addressRoutes from './routes/address'
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
