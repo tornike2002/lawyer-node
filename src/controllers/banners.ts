68a744a8a6d389fa1f050b4d1ae2f9bbecf740a4
tornike2002 <toko.toka2000@gmail.com>
2025-06-24 15:01:24 +0400

    fix prouction banner case sensitive typo

--- src/controllers/banners.ts
+++ src/controllers/banners.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from 'express'
-import Banners from '../models/Banner'
+import Banner from '../models/Banner'
 
 export const getAllBanners = async (_req: Request, res: Response) => {
-  const data = await Banners.find().sort({ createdAt: -1 }).limit(2)
+  const data = await Banner.find().sort({ createdAt: -1 }).limit(2)
   res.status(200).json({ message: 'Banners fetched successfully', data })
 }
 
 export const createBanners = async (req: Request, res: Response) => {
-  const data = await Banners.create(req.body)
+  const data = await Banner.create(req.body)
   res.status(201).json({ message: 'Banners created successfully', data })
 }
 
 export const updateBanners = async (req: Request, res: Response) => {
-  const data = await Banners.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  const data = await Banner.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
   if (!data) {
     res.status(404).json({ message: 'Banners item not found' })
@@ -22,7 +22,7 @@ export const updateBanners = async (req: Request, res: Response) => {
 }
 
 export const deleteBanners = async (req: Request, res: Response) => {
-  const data = await Banners.findByIdAndDelete(req.params.id)
+  const data = await Banner.findByIdAndDelete(req.params.id)
 
   if (!data) {
     res.status(404).json({ message: 'Banner item not found' })
